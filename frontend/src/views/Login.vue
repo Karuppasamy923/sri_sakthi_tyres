55a9a0781509c0970f3ee91f41a891b2f8d530e3
krishna <krishna@aerele.in>
2024-03-14 23:31:39 +0530

    Adding frontend to the app

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="min-h-screen bg-white flex">
+		<div class="mx-auto w-full max-w-sm lg:w-96">
+			<form @submit.prevent="login" class="space-y-6">
+				<label for="email"> Username: </label>
+				<input type="text" v-model="email" />
+				<br />
+				<label for="password"> Password: </label>
+				<input type="password" v-model="password" />
+			</form>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			email: null,
+			password: null,
+		};
+	},
+	inject: ["$auth"],
+	async mounted() {
+		if (this.$route?.query?.route) {
+			this.redirect_route = this.$route.query.route;
+			this.$router.replace({ query: null });
+		}
+	},
+	methods: {
+		async login() {
+			if (this.email && this.password) {
+				let res = await this.$auth.login(this.email, this.password);
+				if (res) {
+					this.$router.push({ name: "Home" });
+				}
+			}
+		},
+	},
+};
+</script>
