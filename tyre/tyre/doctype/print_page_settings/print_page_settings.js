2c9ef93aad947265dc0ca13a9c9df818ab9b2c84
Vijayaragavan07 <vijayaragavanvijay2001@gmail.com>
2024-07-18 11:03:13 +0000

    feat:purchase and sales customizations

---
+++ tyre/tyre/doctype/print_page_settings/print_page_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, krishna@aerele.in and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Print Page Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
